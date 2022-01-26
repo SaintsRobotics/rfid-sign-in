9ab8e1b2dff30bc5a23a02dd9bcf091967c5fa8c
Delaynie <delaynie.mcmillan@gmail.com>
2022-01-25 20:50:36 -0800

    WIP: commiting to push

--- Main.py
+++ Main.py
@@ -1,38 +1,45 @@
 import serial
 import time
 import sqlite3
+import datetime
 
 ser = serial.Serial("/dev/ttyACM0", baudrate=115200, timeout=1)
 print("CONNECTED")
 
-
-def sign_in(id):
-    pass
-
-
-con = sqlite3.connect('sign_ins.db')
+"""con = sqlite3.connect('sign_ins.db')
 cur = con.cursor()
-cur.execute('''CREATE TABLE signins
-               (date tex, trans text, symbol text, qty real, price real)''')
 
-# Insert a row of data
-cur.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+# first name last name, in/out, ID, 
+def sign_in(id):
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS signins ([in] BOOL, [date] INT, [id] INT);")
+    cur.execute(
+        f"INSERT INTO signins (date, id) VALUES(strftime('%s','now'), {id});")
 
-# Save (commit) the changes
-con.commit()
+    # if (SELECT * FROM MyTable WHERE date(datetime(DateColumn / 1000 , 'unixepoch')) = date('now'))
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-con.close()
+    con.commit()
+    print("cURRENT")
+    for x in cur.execute("select * from signins;"):
+        print(x)
 
+"""
 while True:
     resp = ser.read_until("\n").decode("utf-8")
     if resp:
         resp = resp.replace("0x", "").replace(" ", "").rstrip()
-        print(resp)
-        sign_in(resp)
+        try:
+            print(int(resp, 16))
+        except:
+            continue
+        sign_in(int(resp, 16))
+        time.sleep(5)
 
 
-for i in range(5):
-    time.sleep(1)
-    ser.write(s.encode('utf-8'))
+# data csv
+#sign in csv
+
+def sign_in():
+
+
+con.close()
--- arduino random code/arduino_running_code/arduino_running_code.ino
+++ arduino random code/arduino_running_code/arduino_running_code.ino
@@ -36,7 +36,38 @@ void setup(void)
     Serial.begin(115200);
 
     pinMode(8, OUTPUT);
-    //f
+    
+    nfc.begin();
+    nfc.SAMConfig();
+}
+
+uint8_t keya[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; //for auth
+uint8_t data[4];                                        //for reading
+//uint8_t write_data[4] = {0}; //data to write - incrementor
+uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0}; // Buffer to store the returned UID
+uint8_t uidLength;                     // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+void loop(void)
+{
+    if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength))
+        // auth block
+        if (nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya))
+        {
+            //Serial.print("[block authenticated]\n");
+            // read block
+            if (nfc.mifareclassic_ReadDataBlock(4, data))
+            {
+                nfc.PrintHex(data, 4);
+                //Serial.println();
+
+                // Wait a bit before reading the card again
+                delay(1000);
+            }
+        }
+}
+
+void f_you() {
+  //f
     tone(8, 880, 100);
     delay(100);
     tone(8, 880, 100);
@@ -79,41 +110,24 @@ void setup(void)
     delay(100);
     tone(8, 880, 100);
     delay(100);
-    //o
-    //u
-    //dash
     tone(8, 880, 300);
     delay(100);
-    //dot
+    tone(8, 880, 300);
+    delay(100);
+    //o
+    tone(8, 880, 300);
+    delay(100);
+    tone(8, 880, 300);
+    delay(100);
+    tone(8, 880, 300);
+    delay(100);
+    //u
     tone(8, 880, 100);
     delay(100);
-    ..-...- -.-.-.- / -.-----..-
+    tone(8, 880, 100);
+    delay(100);
+    tone(8, 880, 300);
+    delay(100);
+    //..-. ..- -.-. -.- / -.-- --- ..-
 
-        nfc.begin();
-    nfc.SAMConfig();
-}
-
-uint8_t keya[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; //for auth
-uint8_t data[4];                                        //for reading
-//uint8_t write_data[4] = {0}; //data to write - incrementor
-uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0}; // Buffer to store the returned UID
-uint8_t uidLength;                     // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-
-void loop(void)
-{
-    if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength))
-        // auth block
-        if (nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya))
-        {
-            //Serial.print("[block authenticated]\n");
-            // read block
-            if (nfc.mifareclassic_ReadDataBlock(4, data))
-            {
-                nfc.PrintHex(data, 4);
-                //Serial.println();
-
-                // Wait a bit before reading the card again
-                delay(1000);
-            }
-        }
 }
